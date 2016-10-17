a73feb9d593fe30d99c5e3069e6960a6584687e8
JoaoFCosta <dinojoaocosta@gmail.com>
2016-10-17 21:52:00 +0100

    Update dogs crawler.
    Update dogs crawler to download the thumbnail of every dog into the
    `data/dogs` folder. The image will have the format `[DOG_ID]
    DOG_NAME.jpg`.
    Please do not worry about refactoring, it will be delt with later.

--- dogs.rb
+++ dogs.rb
@@ -2,33 +2,57 @@
 
 require 'open-uri'
 require 'nokogiri'
+require 'fileutils'
 
-BASE_URL  = "http://www.abra.org.pt"
-DOGS_URL  = "/caes.html"
+BASE_URL    = "http://www.abra.org.pt"
+DOGS_URL    = "/caes.html"
+DOGS_FOLDER = Dir.pwd + "/data/dogs"
 
-page  = Nokogiri::HTML(open(BASE_URL + DOGS_URL))
-links = page.css("div.subCategoryContainer div.subCategory h2 a")
+# Create folder to store dogs data if it doesn't exist already.
+FileUtils.mkdir_p(DOGS_FOLDER) unless File.directory?(DOGS_FOLDER)
 
-links.each do |link|
-  # Open each link and process dog names.
-  page_url  = BASE_URL + link['href'].gsub('ã','a')
-  list_page = Nokogiri::HTML(open(page_url))
+# Request the initial html page.
+page    = Nokogiri::HTML(open(BASE_URL + DOGS_URL))
+links   = page.css("div.subCategoryContainer div.subCategory h2 a")
+threads = []
 
-  # Get number of pages from the pagination links.
-  pagination_links  = list_page.css("div.pagination a")
-  last_page         = pagination_links[-1]['href'].scan(/\d+/).last.to_i / 25
-  current_page      = 0
-  current_url       = page_url + "?start=0"
+for i in 0...links.size do
+  threads << Thread.new {
+    # Open each link and process dog names.
+    page_url  = BASE_URL + links[i]['href'].gsub('ã','a')
+    list_page = Nokogiri::HTML(open(page_url))
 
-  while current_page <= last_page do
-    puts "===================================>"
-    puts("Processing #{current_url}...")
-    puts "===================================>"
-    list_page = Nokogiri::HTML(open(current_url))
-    dog_names = list_page.css("div.catItemHeader h3.catItemTitle a")
+    # Get number of pages from the pagination links.
+    pagination_links  = list_page.css("div.pagination a")
+    last_page         = pagination_links[-1]['href'].scan(/\d+/).last.to_i / 25
+    current_page      = 0
+    current_url       = page_url + "?start=0"
 
-    dog_names.each { |name| puts("\t#{name.content.strip()} \t\t\t[#{name['href'].scan(/\d+/).last.to_i }]") }
-    current_page  += 1
-    current_url   = page_url + "?start=#{current_page * 25}"
-  end
+    while current_page <= last_page do
+      puts "===================================>"
+      puts("Processing #{current_url}...")
+      puts "===================================>"
+      list_page = Nokogiri::HTML(open(current_url))
+      dogs      = list_page.css("div.catItemView")
+
+      dogs.each { |dog|
+        dog.css("div.catItemHeader h3.catItemTitle a").each { |dog_url|
+          dog_name  = dog_url.content().strip()
+          dog_id    = dog_url['href'].scan(/\d+/).last
+          dog_image = dog.css("div.catItemBody div.catItemImageBlock span.catItemImage a img")
+
+          open(BASE_URL + dog_image.first['src']) { |image|
+            File.open(DOGS_FOLDER + "/[#{dog_id}] #{dog_name}.jpg", "wb") do |file|
+              file.puts image.read
+            end
+          } if !dog_image.empty?
+        }
+      }
+
+      current_page  += 1
+      current_url   = page_url + "?start=#{current_page * 25}"
+    end
+  }
 end
+
+threads.each { |thread| thread.join }
